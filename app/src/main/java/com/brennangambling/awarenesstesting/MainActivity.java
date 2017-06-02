be7c4c5a661a2c2c6be1c1c87530d3ae92c2e34c
BrennanGambling <brennan.gambling.dev@gmail.com>
2017-06-02 15:12:16 -0400

    Moved logging code to a single method. Added fence registering and unregistering to onStart and onStop methods. Added FenceBroadcastReceiver. Added BroadcastReceiver registering and unregistering to onStart and onStop methods.

--- app/src/main/java/com/brennangambling/awarenesstesting/MainActivity.java
+++ app/src/main/java/com/brennangambling/awarenesstesting/MainActivity.java
@@ -2,6 +2,11 @@ package com.brennangambling.awarenesstesting;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -13,12 +18,21 @@ import android.widget.Toast;
 import com.google.android.gms.awareness.Awareness;
 import com.google.android.gms.awareness.fence.AwarenessFence;
 import com.google.android.gms.awareness.fence.DetectedActivityFence;
+import com.google.android.gms.awareness.fence.FenceState;
+import com.google.android.gms.awareness.fence.FenceUpdateRequest;
 import com.google.android.gms.awareness.fence.HeadphoneFence;
 import com.google.android.gms.awareness.state.HeadphoneState;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.ResultCallbacks;
+import com.google.android.gms.common.api.Status;
+
+import static com.google.android.gms.internal.zzt.TAG;
 
 public class MainActivity extends Activity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks{
+
+    static private final String FENCE_ACTION = "WK8Gh9mTvve8T92qVtSp";
 
     static private final String VEHICLE_KEY = "tOUNgsjbYL4t2kd2OcTX";
     static private final String BICYCLE_KEY = "fZfKmVtrSzNrks0oooYW";
@@ -50,14 +64,31 @@ public class MainActivity extends Activity implements GoogleApiClient.OnConnecti
     private AwarenessFence unknownFence;
     private AwarenessFence headphoneFence;
 
+    private PendingIntent fencePendingIntent;
+    private IntentFilter fenceIntentFilter;
+    private FenceBroadcastReceiver fenceBroadcastReceiver;
+
+    /*private boolean vehicleState;
+    private boolean bicycleState;
+    private boolean footState;
+    private boolean walkingState;
+    private boolean runningState;
+    private boolean stillState;
+    private boolean tiltingState;
+    private boolean unknownState;
+    private boolean headphoneState;*/
+
     private GoogleApiClient googleApiClient;
+
+    private static void debugLogging(Context context, String tag, String message) {
+        Log.d(tag, message);
+        Toast toast = Toast.makeText(context, tag + ": " + message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        String message = "onCreate called.";
-        Log.d("Main Activity", message);
-        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
-        toast.show();
+        debugLogging(this,"MainActivity", "onCreate called.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //get TextViews for displaying status of each of the following activities.
@@ -95,47 +126,98 @@ public class MainActivity extends Activity implements GoogleApiClient.OnConnecti
         tiltingFence = DetectedActivityFence.during(DetectedActivityFence.TILTING);
         unknownFence = DetectedActivityFence.during(DetectedActivityFence.UNKNOWN);
         headphoneFence = HeadphoneFence.during(HeadphoneState.PLUGGED_IN);
+
+        Intent intent = new Intent(FENCE_ACTION);
+        fencePendingIntent = PendingIntent.getBroadcast(MainActivity.this,
+                10001,
+                intent,
+                0);
+        fenceIntentFilter = new IntentFilter(FENCE_ACTION);
+        fenceBroadcastReceiver = new FenceBroadcastReceiver();
     }
 
     @Override
     protected void onDestroy() {
-        String message = "onDestroy called.";
-        Log.d("Main Activity", message);
-        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
-        toast.show();
+        debugLogging(this, "MainActivity", "onDestroy called.");
         super.onDestroy();
     }
 
     @Override
     protected void onStart() {
-        String message = "onStart called.";
-        Log.d("Main Activity", message);
-        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
-        toast.show();
+        debugLogging(this, "MainActivity", "onStart called.");
         if (googleApiClient != null) {
             googleApiClient.connect();
         }
+
+        Awareness.FenceApi.updateFences(
+                googleApiClient,
+                new FenceUpdateRequest.Builder()
+                        .addFence(VEHICLE_KEY, vehicleFence, fencePendingIntent)
+                        .addFence(BICYCLE_KEY, bicycleFence, fencePendingIntent)
+                        .addFence(FOOT_KEY, footFence, fencePendingIntent)
+                        .addFence(WALKING_KEY, walkingFence, fencePendingIntent)
+                        .addFence(RUNNING_KEY, runningFence, fencePendingIntent)
+                        .addFence(STILL_KEY, stillFence, fencePendingIntent)
+                        .addFence(TILTING_KEY, tiltingFence, fencePendingIntent)
+                        .addFence(UNKNOWN_KEY, unknownFence, fencePendingIntent)
+                        .addFence(HEADPHONE_KEY, headphoneFence, fencePendingIntent)
+                        .build())
+                .setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        if(status.isSuccess()) {
+                            Log.d(TAG, "Fence was successfully registered.");
+                        } else {
+                            Log.e(TAG, "Fence was NOT successfully registered.: " + status);
+                        }
+                    }
+                });
+
+        registerReceiver(fenceBroadcastReceiver, fenceIntentFilter);
         super.onStart();
     }
 
     @Override
     protected void onStop() {
-        String message = "onStop called.";
-        Log.d("Main Activity", message);
-        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
-        toast.show();
+        debugLogging(this, "MainActivity", "onStop called.");
         if (googleApiClient != null && (googleApiClient.isConnected() || googleApiClient.isConnecting())) {
             googleApiClient.disconnect();
         }
+
+        Awareness.FenceApi.updateFences(
+                googleApiClient,
+                new FenceUpdateRequest.Builder()
+                        .removeFence(VEHICLE_KEY)
+                        .removeFence(BICYCLE_KEY)
+                        .removeFence(FOOT_KEY)
+                        .removeFence(WALKING_KEY)
+                        .removeFence(RUNNING_KEY)
+                        .removeFence(STILL_KEY)
+                        .removeFence(TILTING_KEY)
+                        .removeFence(UNKNOWN_KEY)
+                        .removeFence(HEADPHONE_KEY)
+                        .build()).setResultCallback(new ResultCallbacks<Status>() {
+
+
+            @Override
+            public void onSuccess(@NonNull Status status) {
+                Log.d(TAG, "Fences successfully removed.");
+            }
+
+            @Override
+            public void onFailure(@NonNull Status status) {
+                Log.e(TAG, "Fences were NOT successfully removed.");
+            }
+        });
+
+
+        unregisterReceiver(fenceBroadcastReceiver);
         super.onStop();
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
-        String message = "onSaveInstanceState called.";
-        Log.d("Main Activity", message);
-        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
-        toast.show();
+        debugLogging(this, "MainActivity", "onSaveInstanceState called.");
         super.onSaveInstanceState(savedInstanceState);
         savedInstanceState.putString(VEHICLE_KEY, vehicleTextView.getText().toString());
         savedInstanceState.putString(BICYCLE_KEY, bicycleTextView.getText().toString());
@@ -150,10 +232,7 @@ public class MainActivity extends Activity implements GoogleApiClient.OnConnecti
 
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState) {
-        String message = "onRestoreInstanceState called.";
-        Log.d("Main Activity", message);
-        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
-        toast.show();
+        debugLogging(this, "MainActivity", "onRestoreInstanceState called.");
         super.onRestoreInstanceState(savedInstanceState);
         vehicleTextView.setText(savedInstanceState.getString(VEHICLE_KEY));
         bicycleTextView.setText(savedInstanceState.getString(BICYCLE_KEY));
@@ -168,28 +247,42 @@ public class MainActivity extends Activity implements GoogleApiClient.OnConnecti
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        String message = "onConnected called.";
-        Log.d("Main Activity", message);
-        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
-        toast.show();
+        debugLogging(this, "MainActivity", "onConnected called.");
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-        String message = "onConnectionSuspended called.";
-        Log.d("Main Activity", message);
-        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
-        toast.show();
+        debugLogging(this, "MainActivity", "onConnectionSuspended called.");
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        String message = "onConnectionFailed called. Retrying connection.";
-        Log.d("Main Activity", message);
-        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
-        toast.show();
+        debugLogging(this, "MainActivity", "onConnectionFailed called. Retrying connection.");
         if (googleApiClient != null) {
             googleApiClient.connect();
         }
+    }
+
+    class FenceBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            debugLogging(context, "FenceBroadcastReceiver", "onReceive called.");
+            FenceState fenceState = FenceState.extract(intent);
+            if(fenceState.getFenceKey().equals(VEHICLE_KEY)) {
+
+            }
+        }
+
+        private void setState(Context context, String fenceName, TextView textView, FenceState fenceState) {
+            /*switch (fenceState.getCurrentState()) {
+                case FenceState.TRUE:
+                    debugLogging(context, "FenceBroadcastReceiver:", fenceName + " is true.");
+                    break;
+                case FenceStat
+            }*/
+        }
+
+
     }
 }
